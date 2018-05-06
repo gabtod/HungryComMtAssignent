e9cbc95f91fd3a06fd1e698f73e7732eaad204af
Gabriella <Gabriella>
2018-05-06 18:43:46 +0200

    -the website design of main page, about, FAQ, contact up, login, signup,help are fully designed.

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hungry.com.mt - Main Page</title>
+    </head>
+    <body>
+        <img src="logovr1-1.png">
+    </body>
+</html>
